202c1c507659455938caeeee072f797c3447c576
김동진 <dongjin403@naver.com>
2025-03-13 21:41:38 +0900

    feat : MemberEntity 추가

---
+++ src/main/java/com/member/entity/MemberEntity.java
@@ -0,0 +1,43 @@
+package com.member.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String nickName;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public MemberEntity(String email, String password, String nickName) {
+        this.email = email;
+        this.password = password;
+        this.nickName = nickName;
+    }
+
+    @PrePersist
+    public void setCreatedAtNow() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
