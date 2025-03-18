a9ffff90965156e3ecf8c92b1a9f29b27cd9683d
김동진 <dongjin403@naver.com>
2025-03-18 21:36:30 +0900

    chore : 공백제거

--- src/main/java/com/board/board/controller/BlogApiController.java
+++ src/main/java/com/board/board/controller/BlogApiController.java
@@ -8,21 +8,14 @@ import com.board.board.service.BlogService;
 import com.board.config.auth.AuthUtil;
 import com.board.member.entity.MemberEntity;
 import com.board.member.service.MemberService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -39,13 +32,9 @@ public class BlogApiController {
         if (!authUtil.isAuthenticated()) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-
         String email = authUtil.getUser();
         MemberEntity member = memberService.findByEmail(email);
-
-        //request에 memberId 추가해야됌
         request.setMemberId(member.getId());
-
         Article savedArticle = blogService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED)
