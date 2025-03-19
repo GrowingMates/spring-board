9e927f6f375b1da6343e5359a647f5f902fd8664
김동진 <dongjin403@naver.com>
2025-03-19 20:50:50 +0900

    build  : 초기 데이터 세팅

---
+++ src/main/java/com/board/config/DataInitializer.java
@@ -0,0 +1,62 @@
+package com.board.config;
+
+import com.board.board.domain.Article;
+import com.board.board.repository.BlogRepository;
+import com.board.member.entity.MemberEntity;
+import com.board.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataInitializer {
+
+    private final MemberRepository memberRepository;
+    private final BlogRepository blogRepository;
+
+    @Bean
+    public CommandLineRunner initData() {
+        return args -> {
+            // 기본 회원 데이터 생성
+            MemberEntity member1 = MemberEntity.builder()
+                    .email("aa@aa.com")
+                    .password("1234")
+                    .nickName("aa")
+                    .build();
+
+            MemberEntity member2 = MemberEntity.builder()
+                    .email("aa@aa.com2")
+                    .password("1234")
+                    .nickName("aa2")
+                    .build();
+
+            memberRepository.save(member1);
+            memberRepository.save(member2);
+
+            // 기본 게시글 데이터 생성
+            Article article1 = Article.builder()
+                    .title("First Article")
+                    .content("Content of the first article")
+                    .member(member1)
+                    .build();
+
+            Article article2 = Article.builder()
+                    .title("Second Article")
+                    .content("Content of the second article")
+                    .member(member1)
+                    .build();
+
+            Article article3 = Article.builder()
+                    .title("Third Article")
+                    .content("Content of the third article")
+                    .member(member2)
+                    .build();
+
+            blogRepository.save(article1);
+            blogRepository.save(article2);
+            blogRepository.save(article3);
+        };
+    }
+}
