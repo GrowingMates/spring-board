e83c64e28574f65a497db4fc963d5bcb31b6c3cf
김동진 <dongjin403@naver.com>
2025-05-02 11:00:39 +0900

    feat : 공통 정렬 util 클래스 완성  - 현재   - 게시글 : [최신순, 오래된순, 조회순] 정렬 가능   - 댓글 : [최신순, 오래된순] 정렬 가능

--- src/main/java/com/board/controller/ArticleController.java
+++ src/main/java/com/board/controller/ArticleController.java
@@ -7,15 +7,23 @@ import com.board.entity.ArticleEntity;
 import com.board.service.ArticleService;
 import com.config.auth.annotation.AuthenticatedMember;
 import com.util.page.PageResponse;
+import com.util.sort.SortUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -36,8 +44,8 @@ public class ArticleController {
     @GetMapping("")
     public ResponseEntity<PageResponse<ArticleResponse>> findAllArticles(@RequestParam(defaultValue = "0") int page,
                                                                          @RequestParam(defaultValue = "10") int size,
-                                                                         @RequestParam(defaultValue = "createdAt") String sort) {
-        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, sort));
+                                                                         @RequestParam(defaultValue = "latest") String sort) {
+        Pageable pageable = PageRequest.of(page, size, SortUtils.getArticleSort(sort));
 
         Page<ArticleResponse> articlePage = articleService.findAll(pageable)
                 .map(ArticleResponse::withoutContent);
--- src/main/java/com/board/controller/CommentController.java
+++ src/main/java/com/board/controller/CommentController.java
@@ -7,15 +7,23 @@ import com.board.entity.CommentEntity;
 import com.board.service.CommentService;
 import com.config.auth.annotation.AuthenticatedMember;
 import com.util.page.PageResponse;
+import com.util.sort.SortUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -28,8 +36,8 @@ public class CommentController {
     public ResponseEntity<PageResponse<CommentResponse>> findAllComments(@RequestParam Long articleId,
                                                                          @RequestParam(defaultValue = "0") int page,
                                                                          @RequestParam(defaultValue = "10") int size,
-                                                                         @RequestParam(defaultValue = "createdAt") String sort) {
-        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, sort));
+                                                                         @RequestParam(defaultValue = "latest") String sort) {
+        Pageable pageable = PageRequest.of(page, size, SortUtils.getCommentSort(sort));
         Page<CommentResponse> commentPage = commentService.findAllComments(articleId, pageable)
                 .map(CommentResponse::new);
 
