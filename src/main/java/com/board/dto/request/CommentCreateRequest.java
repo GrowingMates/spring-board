7e08bc7de719f6471f53bf944b6e449c41cc3ccc
김동진 <dongjin403@naver.com>
2025-05-08 20:08:06 +0900

    refactor : requestDTO의 Long 변수에도 유효성 체크 어노테이션 추가

--- src/main/java/com/board/dto/request/CommentCreateRequest.java
+++ src/main/java/com/board/dto/request/CommentCreateRequest.java
@@ -1,6 +1,8 @@
 package com.board.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,8 @@ public class CommentCreateRequest {
 
     @NotBlank(message = "내용을 입력해주세요")
     private String content;
+    @NotNull(message = "게시글 ID는 필수입니다")
+    @Min(value = 1, message = "게시글 ID는 1 이상이어야 합니다")
     private Long articleId;
 
     @Builder
